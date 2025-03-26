8d558944ae4300df98a1c2d77f4a380efa652450
Maham-Ahsan <160377217+Maham-Ahsan@users.noreply.github.com>
2025-03-26 14:57:45 +0500

    deployment

--- deployment/streamlit.py
+++ deployment/streamlit.py
@@ -5,4 +5,4 @@ st.title("Simple Streamlit App")
 user_input = st.text_input("Enter some text:")
 
 if st.button("Show Text"):
-    st.write(f"You Entered: {user_input}")
+    st.write(f"You entered: {user_input}")
